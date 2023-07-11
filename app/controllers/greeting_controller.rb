5fc88b3a19502a6b803cbf1629b703ce19ba37e3
NicolasAndrehh <nicolasolaya22@gmail.com>
2023-07-11 11:16:55 -0500

    Create API endpoint for a random greeting

---
+++ app/controllers/greeting_controller.rb
@@ -0,0 +1,7 @@
+class GreetingController < ApplicationController
+
+    def index 
+        @greeting = Greeting.order("RANDOM()").first
+        render json: { message: @greeting.message }, status: :ok
+    end
+end
