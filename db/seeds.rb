f206395f57c7e3e6601f3d51ed8e5d8e169e0de4
NicolasAndrehh <nicolasolaya22@gmail.com>
2023-07-11 10:59:51 -0500

    Add 5 greetings in seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+first_greeting = Greeting.create(message: "Hello!")
+second_greeting = Greeting.create(message: "What's up!")
+third_greeting = Greeting.create(message: "Hey!")
+fourth_greeting = Greeting.create(message: "Good morning!")
+fifth_greeting = Greeting.create(message: "Good evening!")
